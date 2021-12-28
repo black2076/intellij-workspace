b5061df92f53e0a7adcd6164434e4a15a1752246
bongjunkim <bongjun_kim@tmax.co.kr>
2021-12-28 17:28:23 +0900

    web CRUD

--- src/main/java/com/tmax/bjk/springboot/service/posts/PostsService.java
+++ src/main/java/com/tmax/bjk/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.tmax.bjk.springboot.service.posts;
 
 import com.tmax.bjk.springboot.domain.posts.Posts;
 import com.tmax.bjk.springboot.domain.posts.PostsRepository;
+import com.tmax.bjk.springboot.web.dto.PostsListResponseDto;
 import com.tmax.bjk.springboot.web.dto.PostsResponseDto;
 import com.tmax.bjk.springboot.web.dto.PostsSaveRequestDto;
 import com.tmax.bjk.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -23,14 +27,28 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+ id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
 
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/com/tmax/bjk/springboot/web/IndexContoller.java
+++ src/main/java/com/tmax/bjk/springboot/web/IndexContoller.java
@@ -1,13 +1,34 @@
 package com.tmax.bjk.springboot.web;
 
+import com.tmax.bjk.springboot.service.posts.PostsService;
+import com.tmax.bjk.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexContoller {
+    private final PostsService postsService;
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) { // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
+        model.addAttribute("posts",  postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
